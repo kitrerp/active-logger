5d60c3f3c3560a709f867b23df7647fdcd1b55c7
Pratik Karki <pratik.karki@ventur8.io>
2021-05-31 14:40:25 +0545

    Comment out clojure and spec packages
    Signed-off-by: Pratik Karki <pratik.karki@ventur8.io>

--- project.clj
+++ project.clj
@@ -3,11 +3,12 @@
   :url "http://github.com/active-group/active-logger"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [;[org.clojure/clojure "1.10.1"]
                  ;[org.slf4j/slf4j-log4j12 "1.7.9"] ;; breaks things when included in a project where log4j is already included from somewhere else?!
                  [com.taoensso/timbre "5.1.2"]
                  [de.active-group/active-clojure "0.37.1"]
                  [de.active-group/timbre-riemann "0.2.0"]
                  [cheshire "5.10.0"] ;; for logstash 3rd-party-appender of timbre
                  [riemann-clojure-client "0.5.1"]
-                 [org.clojure/spec.alpha "0.2.194"]])
+                 ;[org.clojure/spec.alpha "0.2.194"]
+                 ])
